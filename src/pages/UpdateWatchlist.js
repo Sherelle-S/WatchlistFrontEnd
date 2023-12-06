fd797faff6de828cf237aa1f81b42421f2b03249
Sherelle <sherellie1@hotmail.com>
2023-12-06 12:43:42 +0000

    use effects for PUT has now been added

--- src/pages/UpdateWatchlist.js
+++ src/pages/UpdateWatchlist.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import { Form, Button } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { NavLink } from "react-router-dom";
@@ -7,6 +7,33 @@ import Footer from "../pageStructure/Footer";
 
 
 function UpdateWatchlist() {
+  const [uuid, setUUID] = useState(""); 
+  // use state to handle input from fields 
+  const handleUpdate = async (event) => {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    const formObject = {}
+    formData.forEach((value, key) => {
+      formObject[key] = value;
+    });
+
+    const formDataJson = JSON.stringify(formObject);
+    try {
+      const response = await fetch(`localhost:8080/watchlist/updateEntry/${uuid}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type" : "application/json",
+        },
+        body: formDataJson,
+      });
+
+      const data = await response.json();
+    } catch (error) {
+      console.error("Error", error);
+    }
+  };
+
     return (
         <>
         <Header />
